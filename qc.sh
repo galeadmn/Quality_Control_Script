afe5b038a800da215b837df3841dd111de600a51
galeadmn <31711727+galeadmn@users.noreply.github.com>
2017-09-06 15:28:03 -0700

    Create qc.sh

---
+++ qc.sh
@@ -0,0 +1,219 @@
+#/bin/bash
+# A menu driven shell script sample template
+## ----------------------------------
+# Step #1: Define variables
+# ----------------------------------
+EDITOR=vim
+PASSWD=/etc/passwd
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+
+FASTQC='fastqc --noextract -t 6 '
+MULTIQC='multiqc '
+HISAT2='hisat2 -p 15 '
+
+declare -a firstAlignments
+
+# ----------------------------------
+# Step #2: User defined function
+# ----------------------------------
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+
+format_input() {
+  eval "last_character=${!1: -1: 1}"
+  if [[ $last_character != "/" ]] ;
+        then eval "$1=${!1}/" ;
+  fi
+}
+
+process_fastqc(){
+
+        # Prompt the user for the input file directory
+        read -p "Please enter the path to the fastqc files: " FASTQC_INPUT
+        # Exit if there is no input entered
+        if [[ -z $FASTQC_INPUT ]] ; then
+                return
+        fi
+        # Append a trailing slash if one is abscent
+        format_input FASTQC_INPUT
+        while [ ! -d "$FASTQC_INPUT" ] ; do
+                read -p "$FASTQC_INPUT does not exist. Please try again [hit return to exit]: " FASTQC_INPUT
+                if [[ -z $FASTQC_INPUT ]] ; then
+                        return
+                fi
+        done
+
+        # Prompt the user for the output file directory
+        read -p "Please enter the path to where output should be written [hit return to quit]: " FASTQC_OUTPUT
+        if [[ -z $FASTQC_OUTPUT ]] ; then
+                return
+        fi
+        format_input FASTQC_OUTPUT
+        while [ ! -d "$FASTQC_OUTPUT" ] ; do
+                read -p "$FASTQC_OUTPUT does not exit. Do you wish to create it?" ans
+                if [[ $ans =~ ^[y,Y] ]] ; then
+                        mkdir -p $FASTQC_OUTPUT
+                else
+                        read -p "Please try again [hit return to exit]: " FASTQC_OUTPUT
+                        if [[ -z $FASTQC_OUTPUT ]] ; then
+                                return
+                        fi
+                fi
+        done
+
+        time $FASTQC -o $FASTQC_OUTPUT $FASTQC_INPUT/*.fastq.gz
+        pause
+}
+
+process_multiqc(){
+
+        # Prompt the user for the input file directory
+        read -p "Please enter the path to the fastqc files: " MULTIQC_INPUT
+        # Exit if there is no input entered
+        if [[ -z $MULTIQC_INPUT ]] ; then
+                return
+        fi
+        # Append a trailing slash if one is abscent
+        format_input MULTIQC_INPUT
+        while [ ! -d "$MULTIQC_INPUT" ] ; do
+                read -p "$MULTIQC_INPUT does not exist. Please try again [hit return to exit]: " MULTIQC_INPUT
+                if [[ -z $MULTIQC_INPUT ]] ; then
+                        return
+                fi
+        done
+
+        # Prompt the user for the output file directory
+        read -p "Please enter the path to where output should be written [hit return to quit]: " MULTIQC_OUTPUT
+        if [[ -z $MULTIQC_OUTPUT ]] ; then
+                return
+        fi
+        format_input MULTIQC_OUTPUT
+        while [ ! -d "$MULTIQC_OUTPUT" ] ; do
+                read -p "$MULTIQC_OUTPUT does not exit. Do you wish to create it?" ans
+                if [[ $ans =~ ^[y,Y] ]] ; then
+                        mkdir -p $MULTIQC_OUTPUT
+                else
+                        read -p "Please try again [hit return to exit]: " MULTIQC_OUTPUT
+                        if [[ -z $MULTIQC_OUTPUT ]] ; then
+                                return
+                        fi
+                fi
+        done
+
+        time $MULTIQC -o $MULTIQC_OUTPUT $MULTIQC_INPUT
+        pause
+}
+
+process_hisat2(){
+        # Prompt the user for the index file
+        read -p "Please enter the base name for the index files: " HISAT_INDEX
+        # Exit if there is no input entered
+        if [[ -z $HISAT_INDEX ]] ; then
+                return
+        else
+                TEST_FILE=($HISAT_INDEX*)
+                while [ ! -f $TEST_FILE ] ; do
+                        read -p "$HISAT_INDEX base files do not exist. Please try again [hit return to exit]: " HISAT_INDEX
+                        if [[ -z $HISAT_INDEX ]] ; then
+                                return
+                        fi
+                done
+        fi
+
+        # Prompt the user for the directory containing alignment files
+        read -p "Please enter the path to the first alignment files: " HISAT_ALIGN_PATH
+        # Exit if there is no input entered
+        if [[ -z "$HISAT_ALIGN_PATH" ]] ; then
+                return
+        else
+                while [ ! -d "$HISAT_ALIGN_PATH" ] ; do
+                        read -p "$HISAT_ALIGN_PATH does not exist. Please try again [hit return to exit]: " HISAT_ALIGN_PATH
+                        if [[ -z $HISAT_ALIGN_PATH ]] ; then
+                                return
+                        fi
+                done
+        fi
+        format_input HISAT_ALIGN_PATH
+        PATH_LENGTH=${#HISAT_ALIGN_PATH}
+
+        # Prompt the user for the name of the file to record output
+        read -p "Please enter the name of this dataset : " DATASET
+
+        # Create a temporary file to store intermediate results
+        OUTFILE=$DATASET.hisat2
+        TEMPFILE=temp.hisat2
+        echo $DATASET hisat2 results > $OUTFILE
+        echo ------------------------------   >> $OUTFILE
+
+        # Record the start time of processing
+        START=$(date +%s.%N)
+        # Read the first elements of alignment paired files into an array
+        firstAlignments=($HISAT_ALIGN_PATH*R1_001.fastq.gz)
+        for firstOfPair in "${firstAlignments[@]}"
+        do
+                BaseFile=${firstOfPair%_R1_001.fastq.gz}
+                secondOfPair=$BaseFile"_R2_001.fastq.gz"
+                if [ ! -f $secondOfPair ] ; then
+                        echo $firstOfPair does not have a matching file! >> $OUTFILE
+                        break
+                fi
+                BaseFile=${BaseFile:$PATH_LENGTH}
+                echo >> $OUTFILE
+                echo Alignment pair $BaseFile >> $OUTFILE
+                hisat_command="$HISAT2 -x $HISAT_INDEX -1 $firstOfPair -2 $secondOfPair -S /dev/null  2> $TEMPFILE"
+                eval $hisat_command
+                sed -i -e '5,15d;2,3d' $TEMPFILE
+                sed -i -E ':a ; $!N ; s/\n\s+/ / ; ta ; P ; D' $TEMPFILE
+                cat $TEMPFILE >> $OUTFILE
+        done
+        END=$(date +%s.%N)
+        DIFFERENCE=$(echo "$END - $START" | bc)
+        echo >> $OUTFILE
+        echo "Time to execute was $DIFFERENCE" >> $OUTFILE
+        mutt -s $DATASET" hisat2 analysis finished" uw_galelab_hisat2output@uw.edu < $OUTFILE
+        rm $TEMPFILE
+}
+
+# function to display menus
+show_menus() {
+        clear
+        echo "~~~~~~~~~~~~~~~~~~~~~"
+        echo " M A I N - M E N U"
+        echo "~~~~~~~~~~~~~~~~~~~~~"
+        echo "1. Run FastQC"
+        echo "2. Run MultiQC"
+        echo "3. Run Hisat2"
+        echo "4. Exit"
+}
+# read input from the keyboard and take a action
+# invoke the one() when the user select 1 from the menu option.
+# invoke the two() when the user select 2 from the menu option.
+# Exit when user the user select 3 form the menu option.
+read_options(){
+        local choice
+        read -p "Enter choice [ 1 - 4] " choice
+        case $choice in
+                1) process_fastqc ;;
+                2) process_multiqc ;;
+                3) process_hisat2 ;;
+                4) exit 0;;
+                *) echo -e "${RED}Error...${STD}" && sleep 2
+        esac
+}
+
+# ----------------------------------------------
+# Step #3: Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+trap '' SIGINT SIGQUIT SIGTSTP
+
+# -----------------------------------
+# Step #4: Main logic - infinite loop
+# ------------------------------------
+while true
+do
+
+        show_menus
+        read_options
+done
